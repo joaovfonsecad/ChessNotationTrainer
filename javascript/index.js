c9c9242ab99591d66956c1c6beb42fe9ac65fc7f
joaovfonsecad <joaovfonsecad@gmail.com>
2022-05-12 12:35:07 -0300

    responsividade melhorada para mobile

--- javascript/index.js
+++ javascript/index.js
@@ -675,4 +675,4 @@ invisDiv.onchange = (e) => {
 // }
 
 let vh = window.innerHeight * 0.01;
-document.documentElement.style.setProperty('--vh', `${vh}px`)
+document.documentElement.style.setProperty('--vh',);
